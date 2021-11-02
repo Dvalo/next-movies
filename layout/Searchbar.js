bf58ce4bdc489bae62170fea39bd407725fc655b
Dvalo <guram.dvalishvili99@gmail.com>
2021-11-03 00:30:39 +0400

    Add logo to secondary header for inner pages

--- layout/Searchbar.js
+++ layout/Searchbar.js
@@ -1,9 +1,20 @@
+import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 function Searchbar() {
   return (
     <div className="secondary-searchbar">
+      <div className="secondary-logo">
+        <a href="https://www.themoviedb.org/" target="_blank" rel="noreferrer">
+          <Image
+            src="/logo.svg"
+            alt="TMDB"
+            height={50}
+            width={150}
+          />
+        </a>
+      </div>
       <form
         id="searchMovie"
         className="mov-searchbar"
@@ -11,10 +22,7 @@ function Searchbar() {
         autoComplete="off"
       >
         <button type="submit">
-          <FontAwesomeIcon
-            icon={faSearch}
-            className="icon icon-search"
-          />
+          <FontAwesomeIcon icon={faSearch} className="icon icon-search" />
         </button>
         <input
           type="text"
