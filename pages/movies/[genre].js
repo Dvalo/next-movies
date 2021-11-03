0e5e62f25526b730d94bae563a4aec52bd2a0982
Dvalo <guram.dvalishvili99@gmail.com>
2021-11-03 22:28:55 +0400

    Add title to genre specific page

--- pages/movies/[genre].js
+++ pages/movies/[genre].js
@@ -1,23 +1,37 @@
-import Image from "next/image";
+import Head from "next/head";
 import Layout from "../../layout/Layout";
 import { Container } from "react-bootstrap";
 import { dummy } from "../../utils/dummy";
 import { discoverMoviesByGenre, requests } from "../../utils/requests";
 import ShowcaseItem from "../../components/ShowcaseItem";
 
-function MovieGenre({ results }) {
+function MovieGenre({ genre, results }) {
   return (
-    <Layout>
-      <div className="showcase-wrapper">
-        <Container fluid className="showcase-container">
-          <div className="item-showcase showcase-inner">
-            {results.map((movie) => (
-              <ShowcaseItem item={movie} key={movie.id} />
-            ))}
-          </div>
-        </Container>
-      </div>
-    </Layout>
+    <>
+      <Head>
+        <title>
+          Movies - {genre.charAt(0).toUpperCase()}
+          {genre.slice(1)}
+        </title>
+      </Head>
+      <Layout>
+        <div className="showcase-wrapper">
+          <Container fluid className="showcase-container">
+            <div className="item-showcase showcase-inner">
+              <h1 className="sect-title">
+                <span>{genre}</span> category
+              </h1>
+
+              <div className="showcase-inner-items">
+                {results.map((movie) => (
+                  <ShowcaseItem item={movie} key={movie.id} />
+                ))}
+              </div>
+            </div>
+          </Container>
+        </div>
+      </Layout>
+    </>
   );
 }
 
@@ -32,6 +46,7 @@ export async function getServerSideProps(context) {
 
   return {
     props: {
+      genre: genre,
       results: dummy,
     },
   };
