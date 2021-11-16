149467ea96a709b18453adb644846dc3a22be27c
Dvalo <guram.dvalishvili99@gmail.com>
2021-11-16 22:25:20 +0400

    Searchbar functionality

--- layout/Searchbar.js
+++ layout/Searchbar.js
@@ -1,25 +1,39 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 function Searchbar() {
+  const router = useRouter();
+  const [query, setQuery] = useState("");
+
+  const handleChange = (setValue) => (e) => setValue(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    router.push({
+      pathname: "/search",
+      query: { searchterm: query },
+    });
+  };
+
   return (
     <div className="secondary-searchbar">
       <div className="secondary-logo">
-        <a className="d-flex" href="https://www.themoviedb.org/" target="_blank" rel="noreferrer">
-          <Image
-            src="/logo.svg"
-            alt="TMDB"
-            height={50}
-            width={150}
-          />
+        <a
+          className="d-flex"
+          href="https://www.themoviedb.org/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Image src="/logo.svg" alt="TMDB" height={50} width={150} />
         </a>
       </div>
       <form
         id="searchMovie"
         className="mov-searchbar"
-        action="/search"
-        autoComplete="off"
+        onSubmit={(e) => handleSubmit(e)}
       >
         <button type="submit">
           <FontAwesomeIcon icon={faSearch} className="icon icon-search" />
@@ -29,6 +43,7 @@ function Searchbar() {
           id="search"
           name="search"
           autoComplete="off"
+          onChange={handleChange(setQuery)}
           placeholder="Search for a movie, tv show, actor..."
         />
       </form>
