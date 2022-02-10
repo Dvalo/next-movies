3d86a148d9b86d9dbb0796b25e0f1bd3c5e478b2
Dvalo <guram.dvalishvili99@gmail.com>
2022-02-11 00:29:59 +0400

    Refactor movie inner page with new component

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -1,10 +1,6 @@
 import Head from "next/head";
-import Link from "next/link";
-import Image from "next/image";
 import Layout from "../../layout/Layout";
-import { Container } from "react-bootstrap";
-import MovieTrailer from "../../components/MovieTrailer";
-import CastSlider from "../../components/CastSlider";
+import ShowcaseSingle from "../../components/ShowcaseSingle";
 import {
   fetchMovieCreditsById,
   fetchMovieDetailsById,
@@ -12,7 +8,6 @@ import {
 } from "../../utils/requests";
 
 function MovieSingle({ movieDetails, movieCredits, movieVideos }) {
-
   return (
     <>
       <Head>
@@ -22,62 +17,12 @@ function MovieSingle({ movieDetails, movieCredits, movieVideos }) {
         </title>
       </Head>
       <Layout>
-        <div className="showcase-single">
-          <div className="showcase-image">
-            <Image
-              src={`${process.env.API_IMAGES}${
-                movieDetails.backdrop_path || movieDetails.poster_path
-              }`}
-              alt={movieDetails.original_title}
-              layout="responsive"
-              objectFit="cover"
-              height={690}
-              width={1920}
-            />
-            <div className="showcase-details-wrapper">
-              <Container className="px-3 px-md-5" fluid>
-                <div className="showcase-title">
-                  {movieDetails.original_title}
-                </div>
-                <div className="showcase-details">
-                  <div className="showcase-date">
-                    {new Date(movieDetails.release_date).toDateString()}
-                  </div>
-                  <div className="showcase-length">
-                    {Math.floor(movieDetails.runtime / 60)}hr{" "}
-                    {movieDetails.runtime % 60}min
-                  </div>
-                </div>
-                <div className="showcase-genres">
-                  {movieDetails.genres.map((genre) => (
-                    <Link href={`/movies/${genre.id}`} key={genre.id}>
-                      <a className="showcase-genre">{genre.name}</a>
-                    </Link>
-                  ))}
-                </div>
-                <div className="showcase-desc">{movieDetails.overview}</div>
-                <div className="showcase-trailer-wrapper">
-                  {movieVideos.results &&
-                    movieVideos.results.slice(0, 4).map((video, index) => (
-                      <MovieTrailer
-                        trailer={video}
-                        index={index}
-                        key={video.key}
-                      />
-                    ))}
-                </div>
-              </Container>
-            </div>
-          </div>
-          <div className="showcase-complete-details">
-            <Container className="px-5" fluid>
-              <h1 className="sect-title">
-                <span>Popular</span> cast
-              </h1>
-              <CastSlider cast={movieCredits.cast} />
-            </Container>
-          </div>
-        </div>
+        <ShowcaseSingle
+          type="movie"
+          showcase={movieDetails}
+          videos={movieVideos}
+          cast={movieCredits}
+        />
       </Layout>
     </>
   );
@@ -95,7 +40,7 @@ export async function getServerSideProps(context) {
   if (movieDetails.success !== undefined && !movieDetails.success) {
     return {
       notFound: true,
-    }
+    };
   }
 
   return {
